433cbb3f5524c9c786bd4d84b2b273ecf9ec3692
Ravikathrotiya18 <ravikatharotiya392@gmail.com>
2023-05-03 09:46:20 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const profiles = [
+    {
+      id: '1',
+      name: 'John Doe',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor erat et nulla feugiat, vitae hendrerit eros dictum. Ut vel est eu eros hendrerit pharetra.',
+      age: 25,
+      location: 'New York'
+    },
+    {
+      id: '2',
+      name: 'Jane Doe',
+      bio: 'Nullam sed enim nec lectus rhoncus volutpat. In vel nisl at ex ullamcorper iaculis vel in odio. Nulla lobortis convallis tristique.',
+      age: 30,
+      location: 'Los Angeles'
+    },
+    {
+      id: '3',
+      name: 'Bob Smith',
+      bio: 'Pellentesque eget enim non eros gravida efficitur. Praesent porta nisi eu urna malesuada sagittis. Sed auctor vel sapien a vehicula.',
+      age: 40,
+      location: 'Chicago'
+    }
+  ];
+  
